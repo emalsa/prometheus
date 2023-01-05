e6326c7928bd2ccd4610d2dee60cc6dfb78d30ed
dni <daniele@nicastro.io>
2023-01-05 12:25:09 +0100

    metanet settings

--- web/sites/default/production.settings.php
+++ web/sites/default/production.settings.php
@@ -7,9 +7,9 @@
 
 $databases['default']['default'] = [
   'driver' => 'mysql',
-  'database' => 'ricardoanalytic',
-  'username' => 'ricardoanalytic',
-  'password' => 'MzZTt7mJMlaksd=22dsa3=dsadsa33=)=)*0932091',
+  'database' => 'prometheus',
+  'username' => 'prometheus',
+  'password' => 'MzZaslaksd=22dsa3=dsadsa33=)=)*0091',
   'host' => '127.0.0.1',
   'port' => 3306,
   'prefix' => '',
@@ -29,7 +29,7 @@ if (isset($GLOBALS['request']) && '/web/index.php' === $GLOBALS['request']->serv
   $GLOBALS['request']->server->set('SCRIPT_NAME', '/index.php');
 }
 
-$settings['hash_salt'] = 'whatever-i-like-29292929';
+$settings['hash_salt'] = 'prometheus-2828-54885-halloeuhjdsahjkaskj';
 $settings['file_temp_path'] = '/tmp';
 $settings['file_public_path'] = 'sites/default/files';
 $settings['file_public_base_url'] = 'https://ricardo.nicastro.io/sites/default/files';
